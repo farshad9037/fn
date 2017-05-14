e9fcf16012512704109f454978ef23887fbfa2ad
Farshad <farshad@Farshads-MacBook-Air.local>
2017-05-14 13:26:45 +0530

    Added scrollToSection Method.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,16 @@
-$(document).ready(function () {
-    var home, about, service, team, project, testimonials, blog, contact, documentBody, sections;
 
+var home, about, service, team, project, testimonials, blog, contact, footer, sections,
+    constants = {
+        HEADER_HEIGHT: 60
+    };
+
+$(document).ready(function () {
+    _initVariables();
+    _loadTemplates();
+    footer.hide();
+});
+
+function _initVariables() {
     home = $("#home");
     about = $("#about");
     service = $("#service");
@@ -9,8 +19,10 @@ $(document).ready(function () {
     testimonials = $("#testimonials");
     blog = $("#blog");
     contact = $("#contact");
-    documentBody = $('body');
+    footer = $('#footer');
+}
 
+function _loadTemplates() {
     home.load("partials/home.html");
     about.load("partials/about.html");
     service.load("partials/service.html");
@@ -18,7 +30,15 @@ $(document).ready(function () {
     project.load("partials/project.html");
     testimonials.load("partials/testimonials.html");
     blog.load("partials/blog.html");
-    contact.load("partials/contact.html");
+    contact.load("partials/contact.html", function () {
+        footer.show()
+    });
+    footer.load("partials/footer.html");
+}
 
-});
+function scrollToSection(section) {
+    $("html, body").animate({
+        scrollTop: section.offset().top - constants.HEADER_HEIGHT
+    }, 'medium');
+}
 
