df964a1f86de98c8f82469fac66562e13f2bc151
Farshad <farshad@Farshads-MacBook-Air.local>
2017-05-28 13:47:20 +0530

    Tried profile of Ambani

--- js/main.js
+++ js/main.js
@@ -1,106 +1,51 @@
-
-// var home, about, service, team, project, testimonials, blog, contact, footer, sections,
-//     constants = {
-//         HEADER_HEIGHT: 60
-//     };
-
-// $(document).ready(function () {
-//     _initVariables();
-//     _loadTemplates();
-//     footer.hide();
-// });
-
-// function _initVariables() {
-//     home = $("#home");
-//     about = $("#about");
-//     service = $("#service");
-//     team = $("#team");
-//     project = $("#project");
-//     testimonials = $("#testimonials");
-//     blog = $("#blog");
-//     contact = $("#contact");
-//     footer = $('#footer');
-// }
-
-// function _loadTemplates() {
-//     home.load("partials/home.html");
-//     about.load("partials/about.html");
-//     service.load("partials/service.html");
-//     team.load("partials/team.html");
-//     project.load("partials/project.html");
-//     testimonials.load("partials/testimonials.html");
-//     blog.load("partials/blog.html");
-//     contact.load("partials/contact.html", function () {
-//         footer.show()
-//     });
-//     footer.load("partials/footer.html");
-// }
-
-// function scrollToSection(section) {
-//     section.toggleClass('active');
-//     $("html, body").animate({
-//         scrollTop: section.offset().top - constants.HEADER_HEIGHT
-//     }, 'medium');
-// }
 var app = angular.module('webApp', ['ui.router', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+
 app.run(['$anchorScroll', function ($anchorScroll) {
     $anchorScroll.yOffset = 60;
 }])
-app.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.when('', '/home');
+
+app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    $urlRouterProvider.when('', '/');
     $stateProvider
         .state('home', {
-            url: '/home',
+            url: '/',
             views: {
                 'home': {
-                    templateUrl: 'partials/home.html',
-                    controller: 'HomeController'
+                    templateUrl: 'partials/home.html'
                 },
                 'about': {
-                    templateUrl: 'partials/about.html',
-                    controller: function ($scope) { console.log('about'); }
+                    templateUrl: 'partials/about.html'
                 },
-                'service': {
-                    templateUrl: 'partials/service.html',
-                    controller: function ($scope) { console.log('graph'); }
+                'childhood': {
+                    templateUrl: 'partials/childhood.html'
                 },
                 'footer': {
-                    templateUrl: 'partials/footer.html',
-                    controller: function ($scope) { console.log('graph'); }
+                    templateUrl: 'partials/footer.html'
                 }
             }
         })
+    $locationProvider.html5Mode(true);
 });
-app.controller('commonCtrl', ['$anchorScroll', '$location', '$scope',
+
+app.controller('CommonCtrl', ['$anchorScroll', '$location', '$scope',
     function ($anchorScroll, $location, $scope) {
         $scope.gotoAnchor = function (x) {
             $location.hash(x);
             $anchorScroll();
         };
-    }
-]);
-app.controller('HomeController', ['$scope',
-    function ($scope) {
         $scope.myInterval = 5000;
         $scope.noWrapSlides = false;
         $scope.active = 0;
         $scope.slides = [{
             id: 0,
-            image: 'https://yusuffali.com/media/1190/fsddsf.jpg',
-            heading: 'First Heading'
+            image: 'css/images/ambani_1.png'
         }, {
             id: 1,
-            image: 'https://yusuffali.com/media/1115/yusuffegaila.jpg',
-            heading: 'Second Heading'
-        },{
-            id: 2,
-            image: 'https://yusuffali.com/media/1117/yusuffa-juffair.jpg',
-            heading: 'Third Heading'
+            image: 'css/images/ambani_2.jpg'
         }, {
-            id: 3,
-            image: 'https://yusuffali.com/media/1119/yusuffali-egypt.jpg',
-            heading: 'Fourth Heading'
-        }];
+            id: 2,
+            image: 'css/images/ambani_3.jpg'
+        }]
     }
 ]);
 
