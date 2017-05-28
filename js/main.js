d636df1f85a6ab1033952628afbb63d949329f44
Farshad <farshad@Farshads-MacBook-Air.local>
2017-05-28 15:09:12 +0530

    Added angular-bootstrap lightbox plugin.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('webApp', ['ui.router', 'ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+var app = angular.module('webApp', ['ui.router', 'ngAnimate', 'ngSanitize', 'ui.bootstrap', 'bootstrapLightbox']);
 
 app.run(['$anchorScroll', function ($anchorScroll) {
     $anchorScroll.yOffset = 60;
@@ -16,8 +16,8 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
                 'about': {
                     templateUrl: 'partials/about.html'
                 },
-                'childhood': {
-                    templateUrl: 'partials/childhood.html'
+                'gallery': {
+                    templateUrl: 'partials/gallery.html'
                 },
                 'footer': {
                     templateUrl: 'partials/footer.html'
@@ -27,8 +27,12 @@ app.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 });
 
-app.controller('CommonCtrl', ['$anchorScroll', '$location', '$scope',
-    function ($anchorScroll, $location, $scope) {
+app.config(['$qProvider', function ($qProvider) {
+    $qProvider.errorOnUnhandledRejections(false);
+}]);
+
+app.controller('CommonCtrl', ['$anchorScroll', '$location', '$scope', 'Lightbox',
+    function ($anchorScroll, $location, $scope, Lightbox) {
         $scope.gotoAnchor = function (x) {
             $location.hash(x);
             $anchorScroll();
@@ -46,6 +50,44 @@ app.controller('CommonCtrl', ['$anchorScroll', '$location', '$scope',
             id: 2,
             image: 'css/images/ambani_3.jpg'
         }]
+
+        $scope.images = [
+            {
+                'url': 'https://farm6.staticflickr.com/5830/20552523531_e1efec8d49_k.jpg',
+                'thumbUrl': 'https://farm6.staticflickr.com/5830/20552523531_ef720cd2f1_s.jpg',
+                'caption': 'This image has dimensions 2048x1519 and the img element is scaled to fit inside the window.'
+            },
+            {
+                'url': 'https://farm8.staticflickr.com/7300/12807911134_ff56d1fb3b_b.jpg',
+                'thumbUrl': 'https://farm8.staticflickr.com/7300/12807911134_ff56d1fb3b_s.jpg'
+            },
+            {
+                'url': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_b.jpg',
+                'thumbUrl': 'https://farm1.staticflickr.com/400/20228789791_52fb84917f_s.jpg',
+                'caption': 'The left and right arrow keys are binded for navigation. The escape key for closing the modal is binded by AngularUI Bootstrap.'
+            },
+            {
+                'url': 'https://farm1.staticflickr.com/260/20185156095_912c2714ef_b.jpg',
+                'thumbUrl': 'https://farm1.staticflickr.com/260/20185156095_912c2714ef_s.jpg'
+            },
+            {
+                'url': 'https://farm6.staticflickr.com/5757/20359334789_57316968ed_m.jpg',
+                'thumbUrl': 'https://farm6.staticflickr.com/5757/20359334789_57316968ed_s.jpg',
+                'caption': 'Default minimum modal dimensions (400x200) apply for this image (240x95).'
+            },
+            {
+                'url': 'https://farm1.staticflickr.com/359/18741723375_28c89372d7_c.jpg',
+                'thumbUrl': 'https://farm1.staticflickr.com/359/18741723375_28c89372d7_s.jpg'
+            },
+            {
+                'url': 'https://farm6.staticflickr.com/5606/15425945368_6f6ae945fc.jpg',
+                'thumbUrl': 'https://farm6.staticflickr.com/5606/15425945368_6f6ae945fc_s.jpg'
+            },
+        ];
+
+        $scope.openLightboxModal = function (index) {
+            Lightbox.openModal($scope.images, index);
+        };
     }
 ]);
 
